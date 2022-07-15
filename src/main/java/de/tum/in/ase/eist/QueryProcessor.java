6ffc0241c89a1d41ac9f7816a6784cd5a6e0f155
Yuanzhuo Li <li.yuanzhuo@outlook.com>
2022-07-15 17:16:57 +0800

    largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,9 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QueryProcessor {
 
@@ -11,9 +14,25 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
+        }
+        else if (query.contains("name")) {
            return "BruceLiFake";
-        } else {
+        }
+        else if (query.contains("largest")) {
+            query = query.substring(query.indexOf(':') + 1);
+            String[] temp = query.split(",");
+            int result = 0;
+            int stringValue = 0;
+            for (String s: temp) {
+                s = s.trim();
+                stringValue = Integer.parseInt(s);
+                if (stringValue > result) {
+                    result = stringValue;
+                }
+            }
+            return String.valueOf(result);
+        }
+        else {
             return "";
         }
     }
